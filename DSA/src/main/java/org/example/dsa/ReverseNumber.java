a26ccca4f6ab0e6ece44d927420710926f2ea7e2
Preethi Kumari P <preethi.nisha1999@gmail.com>
2026-08-17 23:53:33 +0530

    reverse number

---
+++ DSA/src/main/java/org/example/dsa/ReverseNumber.java
@@ -0,0 +1,18 @@
+package org.example.dsa;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] agrs){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = sc.nextInt();
+        int ans = 0;
+        while(number > 0){
+            int rem = number % 10;
+            number /= 10;
+            ans = ans * 10 + rem;
+        }
+        System.out.println(ans);
+    }
+}
